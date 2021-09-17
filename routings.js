5155974d3c7b6b642e52b511cd29aff3455fbd06
aleatory-things <83367341+aleatory-things@users.noreply.github.com>
2021-09-17 16:16:07 -0300

    Delete routings.js

--- routings.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const controllers = require('../controllers/controllers')
-const controllerExecute = require('../controllers/controller-execute')
-
-router.get('/', controllers.first)
-router.post('/execute/:name', controllerExecute.execute)
-
-module.exports = router
